db4d348e9e92eb678ed013069d5026e99bbba895
Serhat Morkoç <serhatmorkoc@gmail.com>
2021-04-30 02:18:11 +0300

    --comm

---
+++ SwiftUIMVVMSample1/SwiftUIMVVMSample1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMSample1App.swift
+//  SwiftUIMVVMSample1
+//
+//  Created by Serhat Morkoç on 30.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMSample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
